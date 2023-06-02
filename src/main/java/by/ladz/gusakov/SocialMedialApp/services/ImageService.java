10bb5d796d958ca500dea4760d4f00d8c6a10cf2
vlad <mainvgd@gmail.com>
2023-06-02 20:35:10 +0300

    complete image service implementation

---
+++ src/main/java/by/ladz/gusakov/SocialMedialApp/services/ImageService.java
@@ -0,0 +1,30 @@
+package by.ladz.gusakov.SocialMedialApp.services;
+
+import by.ladz.gusakov.SocialMedialApp.models.Image;
+import by.ladz.gusakov.SocialMedialApp.models.Publication;
+import by.ladz.gusakov.SocialMedialApp.repositories.ImageRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageService {
+
+    private final ImageRepository imageRepository;
+
+    @Autowired
+    public ImageService(ImageRepository imageRepository) {
+        this.imageRepository = imageRepository;
+    }
+
+    @Transactional
+    public void save(Image image){
+        imageRepository.save(image);
+    }
+
+    @Transactional
+    public void deleteByPublication(Publication publication){
+        imageRepository.deleteByPublication(publication);
+
+    }
+}
