183b92387b2b4ae84b84556ed7081be4c08e65fd
vlad <mainvgd@gmail.com>
2023-06-02 20:28:55 +0300

    complete error response implementation for person

---
+++ src/main/java/by/ladz/gusakov/SocialMedialApp/util/PersonErrorResponse.java
@@ -0,0 +1,8 @@
+package by.ladz.gusakov.SocialMedialApp.util;
+
+public class PersonErrorResponse extends ErrorResponse {
+
+    public PersonErrorResponse(String message, long timestamp){
+        super(message, timestamp);
+    }
+}
