37a74a17d64895ecb7e445855180d36aa3f5946b
vlad <mainvgd@gmail.com>
2023-06-02 20:20:19 +0300

    complete DTO class for person implementation

---
+++ src/main/java/by/ladz/gusakov/SocialMedialApp/dto/PersonDTO.java
@@ -0,0 +1,44 @@
+package by.ladz.gusakov.SocialMedialApp.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public class PersonDTO {
+
+    @NotEmpty(message = "Необходимо указать имя пользователя!")
+    @Size(min = 1, max = 20, message = "Длина имени должна составлять от 1 до 20 символов!")
+    private String username;
+
+    @NotEmpty(message = "Необходимо указать адрес электронной почты!")
+    @Email(message = "Некорректный адрес электронной почты!")
+    @Size(min= 1, max = 256, message = "Длина адреса электронной почты не может превышать 256 символов!")
+    private String email;
+
+    @NotEmpty(message = "Необходимо указать пароль!")
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
