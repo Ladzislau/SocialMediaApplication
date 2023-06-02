a2d3544a4267451370054582399e631ab5e52684
vlad <mainvgd@gmail.com>
2023-06-02 20:29:02 +0300

    complete error response implementation for publication

---
+++ src/main/java/by/ladz/gusakov/SocialMedialApp/util/PublicationErrorResponse.java
@@ -0,0 +1,7 @@
+package by.ladz.gusakov.SocialMedialApp.util;
+
+public class PublicationErrorResponse extends ErrorResponse{
+    public PublicationErrorResponse(String message, long timestamp) {
+        super(message, timestamp);
+    }
+}
