6576800192de88fed22f1013422bf1d7888cc95a
vlad <mainvgd@gmail.com>
2023-06-06 01:00:55 +0300

    create exceptions for publication

---
+++ src/main/java/by/ladz/gusakov/SocialMedialApp/exceptions/IncorrectPublicationException.java
@@ -0,0 +1,7 @@
+package by.ladz.gusakov.SocialMedialApp.exceptions;
+
+public class IncorrectPublicationException extends CustomException{
+    public IncorrectPublicationException(String message) {
+        super(message);
+    }
+}
