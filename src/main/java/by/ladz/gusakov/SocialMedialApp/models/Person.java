57b120227fc7d21ea49dfbce9ba9ad60ca8de52c
vlad <mainvgd@gmail.com>
2023-06-06 00:45:50 +0300

    change default error messages for validation

--- src/main/java/by/ladz/gusakov/SocialMedialApp/models/Person.java
+++ src/main/java/by/ladz/gusakov/SocialMedialApp/models/Person.java
@@ -19,14 +19,14 @@ public class Person {
     private int id;
 
     @Column(name = "username")
-    @NotEmpty(message = "Необходимо указать имя пользователя!")
-    @Size(min = 1, max = 20, message = "Длина имени должна составлять от 1 до 20 символов!")
+    @NotEmpty(message = "Необходимо указать имя пользователя")
+    @Size(min = 1, max = 20, message = "Имя пользователя должно содержать от 1 до 20 символов")
     private String username;
 
     @Column(name = "email")
-    @NotEmpty(message = "Необходимо указать адрес электронной почты!")
-    @Email(message = "Некорректный адрес электронной почты!")
-    @Size(max = 256, message = "Длина адреса электронной почты не может превышать 256 символов!")
+    @NotEmpty(message = "Необходимо указать адрес электронной почты")
+    @Email(message = "Некорректный адрес электронной почты")
+    @Size(max = 256, message = "Длина адреса электронной почты не может превышать 256 символов")
     private String email;
 
     @Column(name = "password")
