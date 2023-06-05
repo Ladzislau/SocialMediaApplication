2c5cc183c44d81907b17440bc3ec24153e54073d
vlad <mainvgd@gmail.com>
2023-06-06 00:37:06 +0300

    delete error response for person

--- src/main/java/by/ladz/gusakov/SocialMedialApp/util/PersonErrorResponse.java
+++
@@ -1,8 +0,0 @@
-package by.ladz.gusakov.SocialMedialApp.util;
-
-public class PersonErrorResponse extends ErrorResponse {
-
-    public PersonErrorResponse(String message, long timestamp){
-        super(message, timestamp);
-    }
-}
