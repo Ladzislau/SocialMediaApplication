77b473b455fb26db2b3108823065735dee534f71
vlad <mainvgd@gmail.com>
2023-06-06 00:38:32 +0300

    delete person exceptions

--- src/main/java/by/ladz/gusakov/SocialMedialApp/util/exceptions/PersonNotAuthenticatedException.java
+++
@@ -1,5 +0,0 @@
-package by.ladz.gusakov.SocialMedialApp.util.exceptions;
-
-public class PersonNotAuthenticatedException extends Exception{
-
-}
--- src/main/java/by/ladz/gusakov/SocialMedialApp/util/exceptions/PersonNotCreatedException.java
+++
@@ -1,7 +0,0 @@
-package by.ladz.gusakov.SocialMedialApp.util.exceptions;
-
-public class PersonNotCreatedException extends RuntimeException{
-    public PersonNotCreatedException(String msg){
-        super(msg);
-    }
-}
