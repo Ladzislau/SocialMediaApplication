1427c24c66f8a1f2f8c113271b9d3a3622415d77
vlad <mainvgd@gmail.com>
2023-06-06 01:00:30 +0300

    create NotFriendException

---
+++ src/main/java/by/ladz/gusakov/SocialMedialApp/exceptions/NotFriendException.java
@@ -0,0 +1,7 @@
+package by.ladz.gusakov.SocialMedialApp.exceptions;
+
+public class NotFriendException extends CustomException{
+    public NotFriendException(String message) {
+        super(message);
+    }
+}
