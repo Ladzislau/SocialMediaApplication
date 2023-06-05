dc68d8aa17448e0675f691ecd5931fd49285ebfd
vlad <mainvgd@gmail.com>
2023-06-06 00:40:11 +0300

    create exceptions for publication class

---
+++ src/main/java/by/ladz/gusakov/SocialMedialApp/exceptions/PublicationNotFoundedException.java
@@ -0,0 +1,7 @@
+package by.ladz.gusakov.SocialMedialApp.exceptions;
+
+public class PublicationNotFoundedException extends CustomException{
+    public PublicationNotFoundedException(String message) {
+        super(message);
+    }
+}
