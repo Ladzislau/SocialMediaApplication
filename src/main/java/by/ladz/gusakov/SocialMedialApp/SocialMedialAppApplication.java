30aedcfb0356c2f2c4d91bd91db3c26a5414b152
vlad <mainvgd@gmail.com>
2023-06-06 07:22:28 +0300

    add swagger

--- src/main/java/by/ladz/gusakov/SocialMedialApp/SocialMedialAppApplication.java
+++ src/main/java/by/ladz/gusakov/SocialMedialApp/SocialMedialAppApplication.java
@@ -4,10 +4,15 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
 @SpringBootApplication
-
+@EnableSwagger2
 public class SocialMedialAppApplication {
 
 
@@ -20,4 +25,12 @@ public class SocialMedialAppApplication {
         return new ModelMapper();
     }
 
+    @Bean
+    public Docket swagger() {
+        return new Docket(SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
