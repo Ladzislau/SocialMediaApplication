880126fdcffba1419bd2fe553b330e0e65a16f10
vlad <mainvgd@gmail.com>
2023-10-24 19:48:55 +0300

    rename exception

---
+++ src/main/java/by/ladz/gusakov/SocialMedialApp/exception/FriendshipRequiredException.java
@@ -0,0 +1,7 @@
+package by.ladz.gusakov.SocialMedialApp.exception;
+
+public class FriendshipRequiredException extends CustomException{
+    public FriendshipRequiredException(String message) {
+        super(message);
+    }
+}
--- src/main/java/by/ladz/gusakov/SocialMedialApp/exception/NotFriendException.java
+++
@@ -1,7 +0,0 @@
-package by.ladz.gusakov.SocialMedialApp.exception;
-
-public class NotFriendException extends CustomException{
-    public NotFriendException(String message) {
-        super(message);
-    }
-}
