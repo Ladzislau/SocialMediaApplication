1946adb80a185018acef6737915dd91d613c1ecf
vlad <mainvgd@gmail.com>
2023-10-25 12:42:11 +0300

    add AllArgsConstructor

--- src/main/java/by/ladz/gusakov/SocialMedialApp/dto/UserMessageDTO.java
+++ src/main/java/by/ladz/gusakov/SocialMedialApp/dto/UserMessageDTO.java
@@ -2,9 +2,13 @@ package by.ladz.gusakov.SocialMedialApp.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class UserMessageDTO {
