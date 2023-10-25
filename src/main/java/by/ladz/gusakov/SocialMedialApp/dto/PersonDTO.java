7f22004fc4e0cabec59c35a923de2f84d7ac4b94
vlad <mainvgd@gmail.com>
2023-10-25 12:39:34 +0300

    add AllArgsConstructor

--- src/main/java/by/ladz/gusakov/SocialMedialApp/dto/PersonDTO.java
+++ src/main/java/by/ladz/gusakov/SocialMedialApp/dto/PersonDTO.java
@@ -3,9 +3,13 @@ package by.ladz.gusakov.SocialMedialApp.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class PersonDTO {
