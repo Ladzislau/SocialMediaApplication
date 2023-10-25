0d4e14728009e41e0687a2123a64db86587cf60e
vlad <mainvgd@gmail.com>
2023-10-25 12:44:58 +0300

    add AllArgsConstructor

--- src/main/java/by/ladz/gusakov/SocialMedialApp/dto/AuthenticationDTO.java
+++ src/main/java/by/ladz/gusakov/SocialMedialApp/dto/AuthenticationDTO.java
@@ -1,9 +1,13 @@
 package by.ladz.gusakov.SocialMedialApp.dto;
 
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class AuthenticationDTO {
