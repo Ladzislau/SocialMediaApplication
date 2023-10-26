218f9fb238b34abb1b23abce4270dc5ac6e2f242
vlad <mainvgd@gmail.com>
2023-10-26 11:23:20 +0300

    rename class to ChatNotFoundException

---
+++ src/main/java/by/ladz/gusakov/SocialMedialApp/exception/ChatNotFoundException.java
@@ -0,0 +1,8 @@
+package by.ladz.gusakov.SocialMedialApp.exception;
+
+public class ChatNotFoundException extends CustomException{
+
+    public ChatNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/by/ladz/gusakov/SocialMedialApp/exception/ChatNotFoundedException.java
+++
@@ -1,8 +0,0 @@
-package by.ladz.gusakov.SocialMedialApp.exception;
-
-public class ChatNotFoundedException extends CustomException{
-
-    public ChatNotFoundedException(String message) {
-        super(message);
-    }
-}
